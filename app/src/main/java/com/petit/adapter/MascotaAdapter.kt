c52a5663628ee005f057ee54c002d2cd469a0ad1
Sergio Calderon <scalderon40839@ufide.ac.cr>
2022-03-25 17:35:15 -0600

    Commit Sergio Calderon

---
+++ app/src/main/java/com/petit/adapter/MascotaAdapter.kt
@@ -0,0 +1,56 @@
+package com.petit.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.petit.databinding.MascotaFileBinding
+import com.petit.model.Mascotas
+import com.petit.ui.Macota.MascotaFragment
+import com.petit.ui.Macota.MascotaFragmentDirections
+
+class MascotaAdapter : RecyclerView.Adapter<MascotaAdapter.MascotaViewHolder>() {
+
+    //Lista para almacenar la info de las mascotas registradas en el app
+
+    private var listaMascotas = emptyList<Mascotas>()
+
+    inner class MascotaViewHolder(private val itemBinding: MascotaFileBinding):
+        RecyclerView.ViewHolder(itemBinding.root){
+
+        fun bind(mascota: Mascotas){
+            itemBinding.tvNombre.text = mascota.nombre
+            itemBinding.tvEdad.text = mascota.edad
+            itemBinding.tvRaza.text = mascota.raza
+            itemBinding.vistaFila.setOnClickListener{
+                val action = MascotaFragmentDirections
+                    .actionNavPetsToUpdateMascotaFragment(mascota)
+                itemView.findNavController().navigate(action)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MascotaViewHolder {
+        val itemBinding = MascotaFileBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent, false)
+
+        return MascotaViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: MascotaViewHolder, position: Int) {
+        val mascota = listaMascotas[position]
+        holder.bind(mascota)
+    }
+
+
+    override fun getItemCount(): Int {
+        return listaMascotas.size
+    }
+
+    fun setData(mascota: List<Mascotas>){
+        this.listaMascotas=mascota
+        notifyDataSetChanged()
+    }
+}
---
+++ app/src/main/java/com/petit/repository/MascotaRepository.kt
@@ -0,0 +1,21 @@
+package com.petit.repository
+
+import androidx.lifecycle.MutableLiveData
+import com.petit.data.MascotaDao
+import com.petit.model.Mascotas
+
+class MascotaRepository (private val mascotaDao : MascotaDao) {
+
+    val getAllData: MutableLiveData<List<Mascotas>> = mascotaDao.getAllData()
+
+    fun addMascota(mascota: Mascotas){
+        mascotaDao.saveMascotas(mascota)
+    }
+    fun updateMascota(mascota: Mascotas){
+        mascotaDao.saveMascotas(mascota)
+    }
+    fun deleteMascota(mascota: Mascotas){
+        mascotaDao.saveMascotas(mascota)
+    }
+
+}
