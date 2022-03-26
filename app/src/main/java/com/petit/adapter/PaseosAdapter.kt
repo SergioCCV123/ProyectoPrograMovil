9c73cc6b2d8943f707886c60d0e4264d1637bcbd
alejandro_2002 <aesquivel50098@ufide.ac.cr>
2022-03-25 20:20:17 -0600

    Alejandro

--- app/src/main/java/com/petit/adapter/PaseosAdapter.kt
+++ app/src/main/java/com/petit/adapter/PaseosAdapter.kt
@@ -4,53 +4,54 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import com.petit.databinding.MascotaFileBinding
-import com.petit.model.Mascotas
-import com.petit.ui.Macota.MascotaFragment
+import com.petit.databinding.PaseoFileBinding
+import com.petit.model.Paseos
 import com.petit.ui.Macota.MascotaFragmentDirections
+import com.petit.ui.gallery.PaseoFragmentDirections
 
-class PaseosAdapter : RecyclerView.Adapter<PaseosAdapter.MascotaViewHolder>() {
+class PaseosAdapter : RecyclerView.Adapter<PaseosAdapter.PaseosViewHolder>() {
 
     //Lista para almacenar la info de las mascotas registradas en el app
 
-    private var listaMascotas = emptyList<Mascotas>()
+    private var listaPaseos = emptyList<Paseos>()
 
-    inner class MascotaViewHolder(private val itemBinding: MascotaFileBinding):
+    inner class PaseosViewHolder(private val itemBinding: PaseoFileBinding):
         RecyclerView.ViewHolder(itemBinding.root){
 
-        fun bind(mascota: Mascotas){
-            itemBinding.tvNombre.text = mascota.nombre
-            itemBinding.tvEdad.text = mascota.edad
-            itemBinding.tvRaza.text = mascota.raza
+        fun bind(paseo: Paseos){
+            itemBinding.tvNombre.text = paseo.nombreMascota
+            itemBinding.tvSalida.text = paseo.horaSalida
+            itemBinding.tvLlegada.text = paseo.horaLlegada
+            itemBinding.tvCosto.text = paseo.Costo
             itemBinding.vistaFila.setOnClickListener{
-                val action = MascotaFragmentDirections
-                    .actionNavPetsToUpdateMascotaFragment(mascota)
+                val action = PaseoFragmentDirections
+                    .actionNavGalleryToUpdatePaseoFragment(paseo)
                 itemView.findNavController().navigate(action)
             }
         }
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MascotaViewHolder {
-        val itemBinding = MascotaFileBinding.inflate(
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PaseosViewHolder {
+        val itemBinding = PaseoFileBinding.inflate(
             LayoutInflater.from(parent.context),
             parent, false)
 
-        return MascotaViewHolder(itemBinding)
+        return PaseosViewHolder(itemBinding)
     }
 
-    override fun onBindViewHolder(holder: MascotaViewHolder, position: Int) {
-        val mascota = listaMascotas[position]
-        holder.bind(mascota)
+    override fun onBindViewHolder(holder: PaseosViewHolder, position: Int) {
+        val paseo = listaPaseos[position]
+        holder.bind(paseo)
     }
 
 
     override fun getItemCount(): Int {
-        return listaMascotas.size
+        return listaPaseos.size
     }
 
-    fun setData(mascota: List<Mascotas>){
-        this.listaMascotas=mascota
+    fun setData(paseo: List<Paseos>){
+        this.listaPaseos=paseo
         notifyDataSetChanged()
     }
 }
